99842e4a0666514a9430c9e7e3c0a87008e8a548
jkrayer <jameskrayer@yahoo.com>
2016-05-10 20:59:45 -0400

    Add home page

---
+++ src/app/components/home.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render () {
+    return (
+      <h1>{"Hello I love you lwt me jump in your game."}</h1>
+    );
+  }
+}
