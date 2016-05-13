ec3391126246ecb9209b1905c7c413aa29932f51
jkrayer <jameskrayer@yahoo.com>
2016-05-13 18:19:28 -0400

    Remove data find and onchange render

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,21 +2,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.jsx'
-import ajax from './helpers/ajax'
 import { Router, hashHistory } from 'react-router';
 import Routes from './routes/routes.jsx';
 
-const MapStore = require('./../stores/map-store.jsx');
-let initial = MapStore.getMaps();
-
-function render () {
-  ReactDOM.render(
-    <Router routes={Routes} history={hashHistory} />,
-    document.getElementById('app-mount-point'));
-}
-
-MapStore.onChange(function (data) {
-  initial = data;
-  render();
-});
+ReactDOM.render(
+  <Router routes={Routes} history={hashHistory} />,
+  document.getElementById('app-mount-point')
+);
