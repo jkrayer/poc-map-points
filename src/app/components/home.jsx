891b72bcd43ba47a8e64765b2cadde2f3eedf3fd
jkrayer <jameskrayer@yahoo.com>
2016-05-15 22:30:44 -0400

    Add maps link list

--- src/app/components/home.jsx
+++ src/app/components/home.jsx
@@ -1,14 +1,25 @@
 'use strict';
 
 import React from 'react';
+import { Link } from 'react-router'
 
-export default class App extends React.Component {
+export default class Home extends React.Component {
   constructor(props) {
     super(props);
   }
   render () {
     return (
-      <h1>{"Hello I love you lwt me jump in your game."}</h1>
+      <ul>
+        {
+          this.props.maps.map( m => <li key={m._id}><Link to={'/maps/' + m.urlSafeName}>{m.mapName}</Link></li>)
+        }
+      </ul>
     );
   }
 }
+
+Home.propTypes = {
+  maps: React.PropTypes.arrayOf(
+    React.PropTypes.shape({})
+  )
+};
