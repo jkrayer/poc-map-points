cf26c02fa330a1a904b00419cdccdc12395d9aac
jkrayer <jameskrayer@yahoo.com>
2016-05-15 23:29:30 -0400

    Use Input

--- src/app/components/home.jsx
+++ src/app/components/home.jsx
@@ -2,18 +2,40 @@
 
 import React from 'react';
 import { Link } from 'react-router'
+import Input from './input.jsx';
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit (event) {
+    event.preventDefault();
+    //gather inputs
+    //make urlSafeName
+    //pass to server
+    //optimistic updates
   }
   render () {
+    let lis = this.props.maps.map( (m) => {
+      return (
+        <li key={m._id}>
+          <Link to={'/maps/' + m.urlSafeName}>{m.mapName}</Link>
+        </li>
+      );
+    });
+
     return (
-      <ul>
-        {
-          this.props.maps.map( m => <li key={m._id}><Link to={'/maps/' + m.urlSafeName}>{m.mapName}</Link></li>)
-        }
-      </ul>
+      <div>
+        <ul>
+          {lis}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <Input name={'mapName'} label={'Map Name:'} />
+          <Input name={'imagePath'} label={'Image Path:'} />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     );
   }
 }
