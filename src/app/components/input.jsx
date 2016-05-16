cf0ecb09cddec1e644c46846423f2c3d692e7ea6
jkrayer <jameskrayer@yahoo.com>
2016-05-15 23:10:25 -0400

    Add input module

---
+++ src/app/components/input.jsx
@@ -0,0 +1,34 @@
+'use strict';
+
+import React from 'react';
+
+export default class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: this.props.defaultVal || ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange (event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  render () {
+    return(
+      <label>{this.props.label}
+        <input
+          onChange={this.handleChange}
+          type="text"
+          value={this.state.input}
+        />
+      </label>
+    );
+  }
+}
+
+Input.propTypes = {
+  defaultVal: React.PropTypes.string,
+  label: React.PropTypes.string.isRequired
+};
