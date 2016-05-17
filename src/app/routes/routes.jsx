7062e35e012535e8959fc77d794b74aadf68f68b
jkrayer <jameskrayer@yahoo.com>
2016-05-17 07:39:38 -0400

    Add routes

--- src/app/routes/routes.jsx
+++ src/app/routes/routes.jsx
@@ -2,13 +2,15 @@
 
 import App from '../components/app.jsx';
 import HomeContainer from '../components/home-container.jsx';
+import MapContainer from '../components/map-container.jsx';
 
 const Routes = {
   path: '/',
   component: App,
   indexRoute: { component: HomeContainer },
   childRoutes: [
-    { path: 'map/:urlString', component: Map }
+    { path: 'maps/', component: HomeContainer },
+    { path: 'maps/:urlString', component: MapContainer }
   ]
 };
 
