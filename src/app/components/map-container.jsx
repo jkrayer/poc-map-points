34d45b03c6514eb2fa835495352a1a68a6aeacdc
jkrayer <jameskrayer@yahoo.com>
2016-05-19 17:30:39 -0400

    Fix store references

--- src/app/components/map-container.jsx
+++ src/app/components/map-container.jsx
@@ -2,17 +2,17 @@
 
 import React from 'react';
 import LocationMap from './location-map.jsx';
-import MapActions from '../../stores/map-store.jsx';
+import MapStore from '../../stores/map-store.jsx';
 
 export default class MapContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { locationMap: MapActions.getMap(props.params.mapId) };
+    this.state = { locationMap: MapStore.getMap(this.props.params.mapId) };
   }
   componentDidMount () {
     let self = this;
     MapStore.onChange(function (data) {
-      self.setState({ locationMap: MapActions.getMap(props.params.mapId) });
+      self.setState({ locationMap: MapStore.getMap(props.params.mapId) });
     });
   }
   render () {
