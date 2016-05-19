a27f16250a7b161b1bc32c479d49c91f15fdb2ce
jkrayer <jameskrayer@yahoo.com>
2016-05-19 09:02:23 -0400

    Add post at /api/maps

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var Map = require('../models/map');
 
 module.exports = function (app) {
@@ -5,6 +7,13 @@ module.exports = function (app) {
     .get(function(req, res){
       Map.find(function (error, doc) {
         res.send(doc);
+      });
+    })
+    .post(function(req, res){
+      let map = req.body;
+      let locationMap = new Map(map);
+      locationMap.save(function (error, data) {
+          res.status(300).send();
       });
     });
 }
