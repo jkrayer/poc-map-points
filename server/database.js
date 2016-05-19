f6572f39a3326180c44e99eb2ce2205ed4a1d206
jkrayer <jameskrayer@yahoo.com>
2016-05-19 09:01:30 -0400

    Comment out re-init db code

--- server/database.js
+++ server/database.js
@@ -4,18 +4,17 @@ var Map = require('./models/map');
 Mongoose.connect('mongodb://localhost/mappoints', function () {
   console.log('connected to mongodb');
 
-  Mongoose.connection.db.dropDatabase();
-
-  var items = [
-    {
-      mapName: 'Norway',
-      imagePath: '/img/norway.png',
-      urlSafeName: 'norway'
-    }
-  ];
-
-  items.forEach(function (item) {
-    new Map(item).save();
-  });
+//  Mongoose.connection.db.dropDatabase();
+//  var items = [
+//    {
+//      mapName: 'Norway',
+//      imagePath: '/img/norway.png',
+//      urlSafeName: 'norway'
+//    }
+//  ];
+//
+//  items.forEach(function (item) {
+//    new Map(item).save();
+//  });
 
 });
