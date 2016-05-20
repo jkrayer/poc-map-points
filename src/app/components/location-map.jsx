d32ea79d1055f4f700bf5ebf3289c3340e245f66
jkrayer <jameskrayer@yahoo.com>
2016-05-19 21:41:47 -0400

    Remove test copy

--- src/app/components/location-map.jsx
+++ src/app/components/location-map.jsx
@@ -10,7 +10,6 @@ export default class LocationMap extends React.Component {
     let { imagePath, mapName } = this.props.locationMap;
     return (
       <div>
-        <h1>{"Location Map"}</h1>
         <img
             alt={mapName}
             src={imagePath}
