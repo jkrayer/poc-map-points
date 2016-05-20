7dece799f82759c63b61b18c1f12760174f3f6b1
jkrayer <jameskrayer@yahoo.com>
2016-05-20 09:04:20 -0400

    Add "addPin" test control

--- src/app/components/location-map.jsx
+++ src/app/components/location-map.jsx
@@ -1,15 +1,52 @@
 'use strict';
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class LocationMap extends React.Component {
   constructor(props) {
     super(props);
+    this.handleAddPin = this.handleAddPin.bind(this);
+    this.state = {
+      addPinOn: false
+    };
+  }
+  handleAddPin (event) {
+    let { addPinOn } = this.state;
+
+    //ignore second click
+    if (addPinOn) {
+      return;
+    }
+
+    //change mouse cursor
+    this.setState({
+      addPinOn: !addPinOn
+    });
+    //listen for next click event
+    //add pin at the selected location
   }
   render () {
     let { imagePath, mapName } = this.props.locationMap;
+    let wrapperClass = null;
+    let btnClass = null;
+
+    if (this.state.addPinOn) {
+      let wrapperClass = ' cursor-pin';
+      let btnClass = 'btn-state-down';
+    }
+
     return (
-      <div>
+      <div className={"location-map-wrapper" + wrapperClass}>
+        <div className="control-panel">
+          <button
+              className={btnClass}
+              onClick={this.handleAddPin}
+              type="button"
+          >
+             {"Add Pin"}
+          </button>
+        </div>
         <img
             alt={mapName}
             src={imagePath}
