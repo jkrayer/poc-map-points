ad7725aaf5658d9f91d0a2db993af8c8516bb05d
jkrayer <jkr@trustpilot.com>
2016-05-24 10:44:48 -0400

    Remove code

--- src/app/components/pin-container.jsx
+++ src/app/components/pin-container.jsx
@@ -10,16 +10,12 @@ export default class PinContainer extends React.Component {
     this.state = { pins: PinStore.getMapPins(this.props.mapId) };
   }
   componentDidMount () {
-console.log(13, this.props);
     var self = this;
     PinStore.onChange(function (data) {
       self.setState({
         pins: PinStore.getMapPins(self.props.mapId)
       });
     });
-  }
-  componentWillReceiveProps () {
-    console.log(22);
   }
   render () {
     return (
