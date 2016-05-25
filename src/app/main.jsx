675e971d1ebf6ea613b8d2c3c3440dae01422c47
jkrayer <jkr@trustpilot.com>
2016-05-25 15:01:02 -0400

    Use browserHistory

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,10 +2,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, hashHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import Routes from './routes/routes.jsx';
 
 ReactDOM.render(
-  <Router routes={Routes} history={hashHistory} />,
+  <Router routes={Routes} history={browserHistory} />,
   document.getElementById('app-mount-point')
 );
