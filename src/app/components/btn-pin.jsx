18c68b45b2d1b55fd6026499abc20bfdd6e71e23
jkrayer <jkr@trustpilot.com>
2016-06-15 16:34:57 -0400

    Move "Add Pin" functionality to its own module

---
+++ src/app/components/btn-pin.jsx
@@ -0,0 +1,77 @@
+'use strict';
+
+import React from 'react';
+import PinActions from '../../actions/pins-action-creator.jsx';
+
+export default class BtnPin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddPin = this.handleAddPin.bind(this);
+    this.addPin = this.addPin.bind(this);
+    this.state = {
+      addPinOn: false
+    };
+  }
+  handleAddPin () {
+    let { addPinOn } = this.state;
+
+    //ignore second click
+    if (addPinOn) {
+      return;
+    }
+    //change mouse cursor
+    this.setState({
+      addPinOn: true
+    });
+    //listen for next click event
+    document.addEventListener('mousedown', this.addPin);
+    //add pin at the selected location
+  }
+  addPin (event) {
+    let pin;
+
+    this.setState({
+      addPinOn: false
+    });
+
+    if (this.props.pin) {
+      pin = Object.assign({}, this.props.pin, {x: event.offsetX, y: event.offsetY});
+      PinActions.update(pin);
+    } else {
+      pin = {
+        mapId: this.props.mapId,
+        x: event.offsetX,
+        y: event.offsetY
+      };
+      PinActions.add(pin);
+    }
+
+    document.removeEventListener('mousedown', this.addPin);
+  }
+  render () {
+    //let wrapperClass = '';
+    let btnClass = '';
+
+    if (this.state.addPinOn) {
+      //wrapperClass = ' cursor-pin';
+      btnClass = 'btn-state-down';
+    }
+    //<div className={"location-map-wrapper" + wrapperClass}>
+
+    return (
+      <button
+          className={btnClass}
+          onClick={this.handleAddPin}
+          type="button"
+      >
+          {this.props.text}
+      </button>
+    );
+  }
+}
+
+BtnPin.propTypes = {
+  mapId: React.PropTypes.string.isRequired,
+  pin: React.PropTypes.shape({}),
+  text: React.PropTypes.string.isRequired
+};
